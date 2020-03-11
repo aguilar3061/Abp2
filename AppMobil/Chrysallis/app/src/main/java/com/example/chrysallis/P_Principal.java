d0f9c06726bf55d310441610ec6612e54eb0db9e
aguilar3061 <56089247+aguilar3061@users.noreply.github.com>
2020-03-11 13:51:16 +0100

    crear estructura

--- AppMobil/Chrysallis/app/src/main/java/com/example/chrysallis/P_Principal.java
+++ AppMobil/Chrysallis/app/src/main/java/com/example/chrysallis/P_Principal.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class P_Principal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.p_principal);
     }
 }
---
+++ AppMobil/Chrysallis/app/src/main/java/com/example/chrysallis/P_VerEventon.java
@@ -0,0 +1,4 @@
+package com.example.chrysallis;
+
+public class P_VerEventon {
+}
