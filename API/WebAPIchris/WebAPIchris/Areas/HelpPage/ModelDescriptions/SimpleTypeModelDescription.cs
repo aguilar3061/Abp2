f6306a74392c5c568c5dceaa4d57dd39c9f2a7dc
aguilar3061 <56089247+aguilar3061@users.noreply.github.com>
2020-04-15 13:03:04 +0200

    borrar apii

--- API/WebAPIchris/WebAPIchris/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebAPIchris.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- API/WebAPIchris/WebAPIchris/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPIchris.WebApiApplication" Language="C#" %>
