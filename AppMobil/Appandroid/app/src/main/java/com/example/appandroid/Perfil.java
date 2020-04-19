ec72c8c87581ef8f39080ba3009d082140ca5d12
David Planas Noguer <49083907+davidplanas@users.noreply.github.com>
2020-04-19 16:00:54 +0200

    Botones cambiar contraseña y cerrar sesion

--- AppMobil/Appandroid/app/src/main/java/com/example/appandroid/Perfil.java
+++ AppMobil/Appandroid/app/src/main/java/com/example/appandroid/Perfil.java
@@ -1,9 +1,13 @@
 package com.example.appandroid;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +15,35 @@ import androidx.fragment.app.Fragment;
 
 public class Perfil extends Fragment {
 
+
+    Button btnModPassword, btnLogout;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_perfil,container,false);
+
+
+        View v = inflater.inflate(R.layout.activity_perfil, container, false);
+
+        btnModPassword = v.findViewById(R.id.btnModPassword);
+
+        btnModPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), ConfirmarContrasenya.class));
+            }
+        });
+
+        btnLogout = v.findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), Login.class));
+            }
+        });
+
+        return v;
+
     }
 }
